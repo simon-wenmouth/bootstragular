b0b4b399ab256a79f2abf4324cbcd4401156d53e
Simon Wenmouth <simon-wenmouth@users.noreply.github.com>
2015-06-27 17:08:47 -0500

    style with bootstrap sourced from bower

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,12 +9,29 @@ module.exports = function(grunt) {
             src: '**',
             dest: 'build/',
             expand: true
+          },
+          bootstrap: {
+            cwd: 'bower_components/bootstrap/fonts/',
+            src: '**',
+            dest: 'build/fonts/',
+            expand: true
           }
+        },
+        less: {
+            development: {
+                options: {
+                    paths: ["less/", "bower_components/bootstrap/less/"]
+                },
+                files: {
+                    "build/css/theme.css": "less/theme.less"
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-less');
 
-    grunt.registerTask('default', ['copy:public']);
+    grunt.registerTask('default', ['copy:public', 'less:development', 'copy:bootstrap']);
 
 };
